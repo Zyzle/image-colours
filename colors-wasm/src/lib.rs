f27acaf169fcab835dd9ea36eb762082de18141a
Colin McCulloch <colin@zyzle.dev>
2022-10-18 02:26:30 +0100

    cleanup

--- colors-wasm/src/lib.rs
+++ colors-wasm/src/lib.rs
@@ -1,15 +1,9 @@
 mod utils;
 
-extern crate serde_json;
-
-use std::fmt::Display;
-
+use serde_derive::{Deserialize, Serialize};
 use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-
-#[macro_use]
-extern crate serde_derive;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -22,12 +16,6 @@ struct Color {
     r: i32,
     g: i32,
     b: i32,
-}
-
-impl Display for Color {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {}, {})", self.r, self.b, self.g)
-    }
 }
 
 #[wasm_bindgen]
@@ -51,7 +39,7 @@ pub fn find_colors(color_data: JsValue, k_clusters: JsValue) -> JsValue {
         }
         console::time_end_with_label("Calc distance shift");
 
-        distance_shift = distance_shift / new_clusters.len() as f32;
+        distance_shift /= new_clusters.len() as f32;
 
         clusters = new_clusters;
 
@@ -63,14 +51,10 @@ pub fn find_colors(color_data: JsValue, k_clusters: JsValue) -> JsValue {
         distance_shift = 0_f32;
     }
 
-    let color_strings = clusters
-        .iter()
-        .map(|c| format!("#{:02x}{:02x}{:02x}", c.r, c.g, c.b))
-        .collect::<Vec<String>>();
-
     JsValue::from(
-        color_strings
-            .into_iter()
+        clusters
+            .iter()
+            .map(|c| format!("#{:02x}{:02x}{:02x}", c.r, c.g, c.b))
             .map(|s| JsValue::from(&s[..]))
             .collect::<js_sys::Array>(),
     )
@@ -79,17 +63,17 @@ pub fn find_colors(color_data: JsValue, k_clusters: JsValue) -> JsValue {
 fn calc_new_clusters(k_clusters: &Vec<Color>, color_data: &Vec<Color>) -> Vec<Color> {
     let mut new_clusters = vec![vec![]; k_clusters.len()];
 
-    for i in color_data {
+    for color in color_data {
         let distances = k_clusters
             .iter()
-            .map(|k| calc_euclidean_dist(k, i))
+            .map(|k| calc_euclidean_dist(k, color))
             .collect::<Vec<f32>>();
 
         let min_distance = distances.iter().fold(f32::INFINITY, |a, &b| a.min(b));
 
         let selected_k = distances.iter().position(|&r| r == min_distance).unwrap();
 
-        new_clusters[selected_k].push(i);
+        new_clusters[selected_k].push(color);
     }
 
     new_clusters
@@ -115,13 +99,7 @@ fn calc_new_clusters(k_clusters: &Vec<Color>, color_data: &Vec<Color>) -> Vec<Co
 }
 
 fn calc_euclidean_dist(p: &Color, q: &Color) -> f32 {
-    let rs = i32::pow(p.r - q.r, 2);
-    let gs = i32::pow(p.g - q.g, 2);
-    let bs = i32::pow(p.b - q.b, 2);
-
-    let result = f32::sqrt((rs + gs + bs) as f32);
-
-    result
+    f32::sqrt((i32::pow(p.r - q.r, 2) + i32::pow(p.g - q.g, 2) + i32::pow(p.b - q.b, 2)) as f32)
 }
 
 #[cfg(test)]
