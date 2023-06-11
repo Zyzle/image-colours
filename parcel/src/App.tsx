0e248dee4ee8527277cbf67ffaaf18d433480004
zyzle <colin.mcculloch@komi.io>
2023-06-11 15:12:19 +0100

    tmp

--- parcel/src/App.tsx
+++ parcel/src/App.tsx
@@ -1,6 +1,8 @@
 import { useCallback, useState } from 'react';
-import { Dropzone } from './components/Dropzone';
 import { Tab } from '@headlessui/react';
+
+import init, { ImageKmeans } from '@zyzle/image-kmeans';
+import { Dropzone } from './components/Dropzone';
 import { EnginePanel } from './components/EnginePanel';
 
 function classNames(...classes) {
@@ -8,22 +10,78 @@ function classNames(...classes) {
 }
 
 export function App() {
-	const handleDroppedFile = useCallback((ibm: ImageBitmap) => {
-		console.log('FILE IS', ibm);
-	}, []);
-
-	let [tabs] = useState([
-		{ label: 'Wasm (fixed K)', engine: 'wasm-fixed' },
-		{ label: 'Wasm (derrived K)', engine: 'wasm-dyn' },
-		{ label: 'JS (danger slow)', engine: 'js' },
+	const [wasmInstance, setWasmInstance] = useState<ImageKmeans>(null);
+	const [tabs, setTabs] = useState([
+		{ label: 'Wasm (fixed K)', engine: 0, swatches: [] },
+		{ label: 'Wasm (derrived K)', engine: 1, swatches: [] },
+		{ label: 'JS (danger slow)', engine: 2, swatches: [] },
 	]);
 
+	const handleDroppedFile = useCallback(
+		(ibm: ImageBitmap) => {
+			const makeWasm = async (ibm: ImageBitmap) => {
+				await init();
+				const canvas = new OffscreenCanvas(ibm.width, ibm.height);
+				const ctx = canvas.getContext('2d');
+				ctx.canvas.height = ibm.height;
+				ctx.canvas.width = ibm.width;
+				ctx.drawImage(ibm, 0, 0);
+
+				const wasmInstance = new ImageKmeans(ctx, ibm.width, ibm.height);
+				setWasmInstance(wasmInstance);
+			};
+
+			makeWasm(ibm);
+		},
+		[setWasmInstance]
+	);
+
+	const doRun = useCallback(
+		engine => {
+			const doAsyncRun = async engine => {
+				let results;
+				switch (engine) {
+					case 0:
+						results = await wasmInstance.with_fixed_k_number(8);
+						break;
+					case 1:
+						results = await wasmInstance.with_derived_k_number();
+						break;
+					case 2:
+						break;
+				}
+				console.log('RESULTS', results);
+				tabs.find(t => t.engine === engine).swatches = results.clusters;
+			};
+			// const result = wasmInstance.with_fixed_k_number(8);
+			// console.log('RESULT', result);
+			doAsyncRun(engine);
+		},
+		[wasmInstance, tabs, setTabs]
+	);
+
 	return (
-		<main className="h-screen flex flex-col items-center p-5 gap-5 bg-gradient-to-b from-cyan-300 to-blue-600">
-			<h1 className="font-bold text-3xl text-slate-700">Image colours with K-means clustering</h1>
-			<Dropzone droppedFile={handleDroppedFile} />
-			<div className="w-full max-w-md">
-				<Tab.Group>
+		<main className="min-h-screen grid grid-cols-2 justify-items-center place-content-around place-items-center p-5 gap-5 bg-gradient-to-b from-cyan-300 to-blue-600">
+			<div className="max-w-md">
+				{/* <h1 className="font-bold text-3xl text-slate-700">Image colours with K-means clustering</h1> */}
+				<Dropzone droppedFile={handleDroppedFile} />
+			</div>
+			<div className="max-w-md rounded-xl bg-white p-3 w-full shadow-md">
+				<div className="flex flex-col gap-5">
+					<button className="rounded-full text-white bg-blue-500 hover:bg-blue-700 px-5 py-2" onClick={() => onGo(engine)}>
+						Go!
+					</button>
+					{/* <div className="grid grid-cols-4 gap-2 justify-items-stretch content-around">
+						{swatches.length
+							? swatches.map(s => (
+									<div key={s} className="rounded-md bg-slate-500 p-5">
+										s
+									</div>
+							  ))
+							: null}
+					</div> */}
+				</div>
+				{/* <Tab.Group>
 					<Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
 						{tabs.map((tab, idx) => (
 							<Tab
@@ -47,11 +105,11 @@ export function App() {
 									'rounded-xl bg-white p-3',
 									'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
 								)}>
-								<EnginePanel engine={tab.engine} />
+								<EnginePanel engine={tab.engine} onGo={doRun} swatches={tab.swatches} />
 							</Tab.Panel>
 						))}
 					</Tab.Panels>
-				</Tab.Group>
+				</Tab.Group> */}
 			</div>
 		</main>
 	);
--- parcel/src/components/EnginePanel/EnginePanel.tsx
+++ parcel/src/components/EnginePanel/EnginePanel.tsx
@@ -1,30 +1,55 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
+
+// import init, { ImageKmeans } from '@zyzle/image-kmeans';
 
 export type EnginePanelProps = {
-	engine: any;
+	engine: number;
+	swatches: string[];
+	onGo: (engine: number) => void;
 };
 
-const swatches = [1, 2, 3, 4, 5, 6, 7, 8];
+const EnginePanel: FC<EnginePanelProps> = ({ engine, swatches = [], onGo }) => {
+	// const wasmURL = new URL('../../../node_modules/@zyzle/image-kmeans/image_kmeans_bg.wasm', import.meta.url);
 
-const EnginePanel: FC<EnginePanelProps> = ({ engine }) => {
-	const wasmURL = new URL('../../../node_modules/@zyzle/image-kmeans/image_kmeans_bg.wasm', import.meta.url);
+	// const wasmModule = WebAssembly.instantiateStreaming(fetch(wasmURL)).then(obj => {
+	// 	console.log('obj', obj);
+	// 	return obj.instance.exports;
+	// });
 
-	const wasmModule = WebAssembly.instantiateStreaming(fetch(wasmURL)).then(obj => {
-		console.log('obj', obj);
-		return obj.instance.exports;
-	});
+	// wasmModule.then(m => console.log(m));
 
-	wasmModule.then(m => console.log(m));
+	// const canvasRef = useRef();
+
+	// useEffect(() => {
+	// 	const doWasm = async () => {
+	// 		await init();
+	// 		if (typeof OffscreenCanvasRenderingContext2D === 'function') {
+	// 			// ImageKmeans.init();
+	// 			console.log(ImageKmeans);
+	// 			const canvas = new OffscreenCanvas(100, 100);
+	// 			const ctx = canvas.getContext('2d');
+	// 			const kmeans = new ImageKmeans(ctx as unknown as CanvasRenderingContext2D, 100, 100);
+	// 			console.log('kmeans', kmeans);
+	// 		}
+	// 	};
+
+	// 	doWasm();
+	// }, [canvasRef]);
 
 	return (
 		<div className="flex flex-col gap-5">
-			<button className="rounded-full text-white bg-blue-500 hover:bg-blue-700 px-5 py-2">Go!</button>
-			<div className="flex flex-row flex-wrap gap-2">
-				{swatches.map(s => (
-					<div key={s} className="rounded-sm bg-slate-500 p-5">
-						#123456
-					</div>
-				))}
+			<button className="rounded-full text-white bg-blue-500 hover:bg-blue-700 px-5 py-2" onClick={() => onGo(engine)}>
+				Go!
+			</button>
+			<div className="grid grid-cols-4 gap-2 justify-items-stretch content-around">
+				{swatches.length
+					? swatches.map(s => (
+							<div key={s} className="rounded-md bg-slate-500 p-5">
+								s
+							</div>
+					  ))
+					: null}
+				{/* <canvas ref={canvasRef} /> */}
 			</div>
 		</div>
 	);
